d295e5ae4ad32bb1a8200f0639f15425c3895af0
AlbertMosella <mosella.albert@gmail.com>
2022-05-14 14:58:02 +0200

    Added load thunk

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
-function App() {}
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadRobotsThunk } from "./redux/thunks/robotsThunks";
+
+function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRobotsThunk());
+  }, [dispatch]);
+}
 
 export default App;
